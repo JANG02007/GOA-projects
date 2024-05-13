4cb38c882fdd65657fc6db96d7257f0a36a873ff
JANG02007 <turkajango313@gmail.com>
2024-05-13 19:44:15 +0300

    commit

--- day 00/homework/0.py
+++ day 00/homework/0.py
@@ -1,6 +1,7 @@
 from turtle import *
 
 width(10)
+speed(10)
 
 left(90)
 forward(200)
@@ -10,11 +11,39 @@ right(90)
 forward(200)
 right(90)
 forward(400)
-
-
-
+right(90)
+forward(200)
 right(45)
-forward
+forward(280)
+right(90)
+forward(280)
+
+penup()
+goto(50, 110)
+pendown()
+
+left(135)
+forward(70)
+right(90)
+forward(60)
+right(90)
+forward(70)
+right(90)
+forward(60)
+
+penup()
+goto(-150, -500)
+pendown()
+right(90)
+forward(500)
+right(90)
+forward(700)
+right(90)
+forward(500)
+
+exitonclick()
+
+
 
 
 
---
+++ day01/homework/1.py
@@ -0,0 +1,6 @@
+name="Jango"
+surname="Turkadze"
+age = 16
+height = 186
+
+print("i am " + name + " " + surname +". I am " + str(age) + " years old. my height is " + str(height)+".")
